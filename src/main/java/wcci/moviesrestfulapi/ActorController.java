97ac23d3edbfbf6ff0d2dc39ff3f1c09c1e26452
Sshipley12 <steph.shipley@yahoo.com>
2019-07-26 16:43:54 -0400

    creation of web layer actor test

--- src/main/java/wcci/moviesrestfulapi/ActorController.java
+++ src/main/java/wcci/moviesrestfulapi/ActorController.java
@@ -1,22 +1,27 @@
 package wcci.moviesrestfulapi;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/api")
 public class ActorController {
 
 	@Autowired
 	private ActorRepository actorRepo;
-	
+
+	@GetMapping("/actors")
 	public Iterable<Actor> findAllActors() {
-		
+
 		return actorRepo.findAll();
 	}
 
 	public Actor findOneActor(Long id) {
-		
+
 		return actorRepo.findById(id).get();
 	}
-
-	
 
 }
